cdb66a23eeb35e18b1b7adf881865e91c328a7fe
Luis Fernando Zatarain Aguilera <luisfernando.zatar@hcl.com>
2020-09-03 11:51:22 -0500

    Install Pug

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'pug');
+
 
 app.get('/', (req, res) => {
+  res.render("index")
+})
+
+app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
 
